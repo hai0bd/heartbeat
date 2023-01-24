d68e971726cadf10c97dfe7a32a5c3e1e059312d
hai0bd <dosonhai2004@gmail.com>
2023-01-24 21:32:49 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  // process bar
+  setTimeout(function () {
+    firstQuestion();
+    $(".spinner").fadeOut();
+    $("#preloader").delay(350).fadeOut("slow");
+    $("body").delay(350).css({
+      overflow: "visible",
+    });
+  }, 2000);
+});
+
+function firstQuestion() {
+  $(".content").hide();
+}
